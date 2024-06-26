ae49ea1ba5d43a34147adf423b7b076fa81c4841
emBers19 <sueeih14@gmail.com>
2024-06-26 14:43:39 +0700

    Update 14:43 26/06/2024 by Hiếu

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,7 @@ import './App.css'
 import YumTravel from './component/YumTravel'
 function App() {
   return (
-<<<<<<< HEAD
     <YumTravel/>
-=======
-    <div>
-      <div>Hello</div>
-    </div>
->>>>>>> bf0057d782dbe63c78e6cbf6690cd238fc467ff2
   )
 }
 
