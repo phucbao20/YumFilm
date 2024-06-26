a4cab6ed5719125b498a7f1092a133f90c0e6359
emBers19 <sueeih14@gmail.com>
2024-06-26 14:23:30 +0700

    Update 14:23 26/06/2024 by Hiếu

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import YumTravel from './component/YumTravel'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>adsdas</div>
+    <YumTravel/>
   )
 }
 
