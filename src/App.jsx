38263cc16c0c86cf8329a418771dc928735d58fb
phucbao20 <lnpbao15@gmail.com>
2024-07-09 14:25:53 +0700

    Update 14:25 App bao

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import Conten from './components/Conten/Conten'
-import FoodOrder from './components/FoodOrder/FoodOrder'
 const App = () => {
   return (
     <div className='min-h-screen overflow-hidden'>
@@ -12,12 +10,9 @@ const App = () => {
       {/* <Conten /> */}
       {/* <FoodOrder /> */}
       <Footer />
-<<<<<<< HEAD
 
       <Conten />
     </div>
-=======
->>>>>>> 5e52f84de3b7ede6a4d0e13c77bda677ad457d61
   )
 }
 
