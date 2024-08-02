7473ae4e6093aabdca2e72c39f5810532ff8ad4d
anhda <anhdaovu070430@gmail.com>
2024-08-02 23:43:45 +0700

    CRUD Food, no image - 23:44 2/8/2024 by Dao

--- src/main/java/com/example/yumfilm/repository/FoodRepository.java
+++ src/main/java/com/example/yumfilm/repository/FoodRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FoodRepository extends JpaRepository<Food, Invoice> {
+public interface FoodRepository extends JpaRepository<Food, Integer> {
 }
